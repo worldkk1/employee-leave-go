0b57a16a185aaa0720a9bfc8c576abf9ac541623
Krittamate Khamkom <worldkk@Its-my-MacBook-Air.local>
2024-05-25 10:40:57 +0700

    ✨ update user table

--- internal/app/models/user.go
+++ internal/app/models/user.go
@@ -9,6 +9,8 @@ import (
 type User struct {
 	Id        uuid.UUID `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
 	Name      string    `json:"name" gorm:"type:varchar;not null"`
+	Email     string    `json:"email" gorm:"type:varchar;not null;unique"`
+	Password  string    `json:"password" gorm:"type:varchar;not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
