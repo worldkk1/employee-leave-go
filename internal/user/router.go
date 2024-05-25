eb37c09134fe24880f9c93fcbc389503d52f913d
Krittamate Khamkom <worldkk@Its-my-MacBook-Air.local>
2024-05-25 18:30:31 +0700

    ✨ add edit user api

--- internal/user/router.go
+++ internal/user/router.go
@@ -5,5 +5,6 @@ import "github.com/gin-gonic/gin"
 func SetupRouter(router *gin.RouterGroup) {
 	users := router.Group("/users")
 
-	users.POST("", CreateUser)
+	users.POST("/", CreateUser)
+	users.PATCH("/:id", EditUser)
 }
--- internal/user/service.go
+++ internal/user/service.go
@@ -13,6 +13,10 @@ type CreateUserInput struct {
 	Email string `json:"email" binding:"required"`
 }
 
+type EditUserInput struct {
+	Name string `json:"name"`
+}
+
 func CreateUser(c *gin.Context) {
 	var input CreateUserInput
 	if err := c.ShouldBindJSON(&input); err != nil {
@@ -26,6 +30,24 @@ func CreateUser(c *gin.Context) {
 		Password: "",
 	}
 	database.DB.Create(&user)
+
+	c.JSON(http.StatusOK, gin.H{"data": user})
+}
+
+func EditUser(c *gin.Context) {
+	var input EditUserInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error})
+		return
+	}
+
+	var user models.User
+	if err := database.DB.Where("id = ?", c.Param("id")).First(&user).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "User not found"})
+		return
+	}
+
+	database.DB.Model(&user).Updates(input)
 
 	c.JSON(http.StatusOK, gin.H{"data": user})
 }
