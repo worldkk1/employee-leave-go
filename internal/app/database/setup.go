440c04a7401734e68fd6b2e57c6fc70b1fd97677
Krittamate Khamkom <worldkk@Its-my-MacBook-Air.local>
2024-05-25 10:40:39 +0700

    ✨ use db config from env

--- internal/app/database/setup.go
+++ internal/app/database/setup.go
@@ -1,13 +1,34 @@
 package database
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
 	"github.com/worldkk1/employee-leave-go/internal/app/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func SetupDB() *gorm.DB {
-	dsn := "host=localhost user=postgres password=postgres dbname=employee_leave port=5432"
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("failed to load .env file")
+	}
+
+	port, err := strconv.Atoi(os.Getenv("DATABASE_PORT"))
+	if err != nil {
+		panic("failed to get db config")
+	}
+	var (
+		host     = os.Getenv("DATABASE_HOST")
+		user     = os.Getenv("DATABASE_USERNAME")
+		password = os.Getenv("DATABASE_PASSWORD")
+		dbName   = os.Getenv("DATABASE_NAME")
+	)
+
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", host, port, user, password, dbName)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
